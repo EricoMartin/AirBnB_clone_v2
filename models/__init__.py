6c8b2e1203552cd3dbba9f0e370a3a3e15e345ee
EricoMartin <ericomartin.0785@gmail.com>
2023-12-29 03:19:40 -0800

    sqlalchemy

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,7 +13,7 @@ from os import getenv
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
-    pass
+    storage = DBStorage()
 else:
     storage = FileStorage()
 storage.reload()
